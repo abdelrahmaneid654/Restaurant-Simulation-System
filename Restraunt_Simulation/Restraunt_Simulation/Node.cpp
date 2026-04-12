f86e0039c3b135cae5bdb7aabe754ecf525af67d
Abdelrahman <abdelrahmaneid654@gmail.com>
2026-04-12 23:58:20 +0200

    class node

--- Restraunt_Simulation/Restraunt_Simulation/Node.cpp
+++ Restraunt_Simulation/Restraunt_Simulation/Node.cpp
@@ -1,1 +1,24 @@
 #include "Node.h"
+template<class T>
+Node<T>* Node<T>::getnext()
+{
+
+	return next;
+}
+template<class T>
+void Node<T>::setnext(Node<T>* n)
+{
+	next = n;
+
+}
+
+template<class T>
+T Node<T>::getdata() {
+	return data;
+}
+
+template<class T>
+void Node<T>::setdata(T x)
+{
+	data = x;
+}
--- Restraunt_Simulation/Restraunt_Simulation/Node.h
+++ Restraunt_Simulation/Restraunt_Simulation/Node.h
@@ -1,5 +1,17 @@
 #pragma once
+template<class T>
+
 class Node
 {
+	T data;
+	Node* next;
+public:
+	T getdata();
+	void setdata(T x);
+	Node<T>* getnext();
+	void setnext(Node<T>*n);
+		
+
+		
 };
 
