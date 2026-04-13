3b01eac37c76eca39b8dbf0e4f1e12abe8f734db
Abdelrahman <abdelrahmaneid654@gmail.com>
2026-04-13 12:17:55 +0200

    Queue class

--- Restraunt_Simulation/Restraunt_Simulation/Queue.h
+++ Restraunt_Simulation/Restraunt_Simulation/Queue.h
@@ -1,5 +1,17 @@
 #pragma once
+#include"Node.h"
+template<class T>
 class Queue
 {
+	Node<T>* front;
+	Node<T>* rear;
+public:
+	Queue();
+	bool peek(T& x);
+	bool isempty();
+	void enqueue(T x);
+	bool dequeue(T &x);
+	void print();
+	int getcount();
 };
 
