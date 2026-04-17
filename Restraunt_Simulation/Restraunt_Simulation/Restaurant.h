ca9df85398b0ad6b48d50dfc906eb168396d83b5
Adam Ehab <adam32ehab@gmail.com>
2026-04-17 13:31:26 +0200

    after restaurant class edit again

--- Restraunt_Simulation/Restraunt_Simulation/Restaurant.h
+++ Restraunt_Simulation/Restraunt_Simulation/Restaurant.h
@@ -17,6 +17,8 @@
 #include "Pending_OVC.h"//Derived from Queue class
 #include "RDY_OV.h"//Derived from Queue class
 
+#include "EnumTypes.h"
+
 
 
 
@@ -54,10 +56,9 @@ private:
 	  // 1-Free Chefs
 	Queue <Chef*> Free_CS; //Free Special Chefs
 	Queue <Chef*> Free_CN; //Free Normal Chefs
-	PriQueue <Chef*> Busy_CS;//Busy Special Chefs
-	PriQueue <Chef*> Busy_CS;//Busy Normal Chefs
-	//I choose PriQueue because it has a relation to the time and we must know and implement to them the needed equations
-
+	//I choose PriQueue for busy chefs because it has a relation to the time and we must know and implement to them the needed equations
+	//I delete busy chefs because I will make the order points to the chef and before send order to finished List I will enqueue the chef to the list again.
+	
 	//Scooters 
 	PriQueue<Scooter*> Free_Scooters; //assigned based for the shortest distance
 	PriQueue<Scooter*> Busy_Scooters; //Leaves the list based on the distance they cut back to the restaurant
@@ -100,6 +101,21 @@ private:
 
 	//STILL I do not write the point 6 in the document num 1 page 6 
 
+
+
+	//PRIVATE FUNCTIONS
+private: 
+	//All these functions are for the random simulation
+	bool AreAllOrdersFinishedOrCancelled();
+	Order* CreateRandomOrder( int  ArrivalTime);
+	Chef* CreateRandomChefs(int ChefID);
+	Table* CreateRandomTables(int TableId);
+	Chef* pickRandomChefs();
+	Order* pickRandomOrderFromPendingLists();
+	Order* pickRandomOrderFromReadyLists();
+	Order* FromCookingToReadyByType(Order* pOrder);
+	bool CancelOrder(int id);
+
 public:
 	Restaurant();
 
@@ -117,13 +133,15 @@ public:
 	// Assign ready orders to scooter/table or give to customer (OT orders)
 	void RandomSimulation();
 
+	void AddToPending(Order* pOrder);//I made this to take the order type and put it in the specific list.
+	
 
 
 	void UpdateInterface();
 	//it is implemented only to pass the lists to the UI class1
 
 	int GetCurrentTimestep() const;
-	//Return the current time step need when execute actions .
+	//Return the current time step need when execute actions 
+
 	~Restaurant();
 };
-
