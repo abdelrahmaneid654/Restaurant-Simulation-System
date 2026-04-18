1c50a09064af28b5dd89e11936e421036632301b
Adamba4a777 <adam.abdelsalam06@eng-st.cu.edu.eg>
2026-04-18 13:10:42 +0200

    k

--- Restraunt_Simulation/Restraunt_Simulation/Node.h
+++ Restraunt_Simulation/Restraunt_Simulation/Node.h
@@ -6,63 +6,39 @@ class Node
 	T data;
 	Node* next;
 public:
-	T getdata() {
-		return data;
-	}
-	void setdata(T x) {
-		data = x;
-	}
-	Node<T>* getnext() {
+
+	template<class T>
+	Node<T>* Node<T>::getnext()
+	{
 
 		return next;
 	}
-	void setnext(Node<T>*n) {
+	template<class T>
+	void Node<T>::setnext(Node<T>* n)
+	{
 		next = n;
 
 	}
-	Node() {
+
+	template<class T>
+	Node<T>::Node()
+	{
 		next = nullptr;
 	}
-		
-	Node(T x) {
+	template<class T>
+	Node<T>::Node(T x) {
 		data = x;
 		next = nullptr;
 	}
-		
+
+	template<class T>
+	T Node<T>::getdata() {
+		return data;
+	}
+
+	template<class T>
+	void Node<T>::setdata(T x)
+	{
+		data = x;
+	}
 };
-
-template<class T>
-Node<T>* Node<T>::getnext()
-{
-
-	return next;
-}
-template<class T>
-void Node<T>::setnext(Node<T>* n)
-{
-	next = n;
-
-}
-
-template<class T>
-Node<T>::Node()
-{
-	next = nullptr;
-}
-template<class T>
-Node<T>::Node(T x) {
-	data = x;
-	next = nullptr;
-}
-
-template<class T>
-T Node<T>::getdata() {
-	return data;
-}
-
-template<class T>
-void Node<T>::setdata(T x)
-{
-	data = x;
-}
-
--- Restraunt_Simulation/Restraunt_Simulation/Stack.h
+++ Restraunt_Simulation/Restraunt_Simulation/Stack.h
@@ -6,29 +6,43 @@ class Stack
 {
 	Node<T>* top;
 public:
-	Stack() {
+
+
+	template<class T>
+	Stack<T>::Stack() {
 		top = nullptr;
 
 	}
-	bool peek(T& x) {
+
+	template<class T>
+	bool Stack<T>::peek(T& x)
+	{
 		if (!isempty()) {
 			x = top->getdata();
 			return true;
 		}
 		return false;
 	}
-	bool isempty() {
+	template<class T>
+	bool Stack<T>::isempty()
+	{
 		if (top == nullptr)
 			return true;
 		return false;
 	}
-	void push(T x) {
+
+	template<class T>
+	void Stack<T>::push(T x)
+	{
 		Node<T>* temp = new Node<T>;
 		temp->setdata(x);
 		temp->setnext(top);
 		top = temp;
 	}
-	bool pop(T& x) {
+
+	template<class T>
+	bool Stack<T>::pop(T& x)
+	{
 		if (!isempty()) {
 			Node<T>* temp;
 			temp = top;
@@ -40,14 +54,19 @@ public:
 		}
 		return false;
 	}
-	void print() {
+
+	template<class T>
+	void Stack<T>::print()
+	{
 		Node<T>* temp = top;
 		while (temp) {
 			std::cout << temp->getdata() << " ";
 			temp = temp->getnext();
 		}
 	}
-	int getcount() {
+
+	template<class T>
+	int Stack<T>::getcount() {
 		int count = 0;
 		Node<T>* temp = top;
 		while (temp) {
@@ -56,84 +75,14 @@ public:
 		}
 		return count;
 	}
-	~Stack();
+
+	template<class T>
+	Stack<T>::~Stack()
+	{
+		T x;
+		while (!isempty()) {
+			pop(x);
+		}
+	}
+
 };
-
-template<class T>
-Stack<T>::Stack() {
-	top = nullptr;
-
-}
-
-template<class T>
-bool Stack<T>::peek(T& x)
-{
-	if (!isempty()) {
-		x = top->getdata();
-		return true;
-	}
-	return false;
-}
-template<class T>
-bool Stack<T>::isempty()
-{
-	if (top == nullptr)
-		return true;
-	return false;
-}
-
-template<class T>
-void Stack<T>::push(T x)
-{
-	Node<T>* temp = new Node<T>;
-	temp->setdata(x);
-	temp->setnext(top);
-	top = temp;
-}
-
-template<class T>
-bool Stack<T>::pop(T& x)
-{
-	if (!isempty()) {
-		Node<T>* temp;
-		temp = top;
-		x = top->getdata();
-		top = top->getnext();
-		temp->setnext(nullptr);
-		delete temp;
-		return true;
-	}
-	return false;
-}
-
-template<class T>
-void Stack<T>::print()
-{
-	Node<T>* temp = top;
-	while (temp) {
-		std::cout << temp->getdata() << " ";
-		temp = temp->getnext();
-	}
-}
-
-template<class T>
-int Stack<T>::getcount() {
-	int count = 0;
-	Node<T>* temp = top;
-	while (temp) {
-		count++;
-		temp = temp->getnext();
-	}
-	return count;
-}
-
-template<class T>
-Stack<T>::~Stack()
-{
-	T x;
-	while (!isempty()) {
-		pop(x);
-	}
-}
-
-
