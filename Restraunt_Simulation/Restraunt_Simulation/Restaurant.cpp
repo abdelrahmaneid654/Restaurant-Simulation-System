7030a0f5a80ac9ba72b20427ca25831162c7a646
Adamba4a777 <adam.abdelsalam06@eng-st.cu.edu.eg>
2026-05-02 15:46:37 +0300

    f

--- Restraunt_Simulation/Restraunt_Simulation/Restaurant.cpp
+++ Restraunt_Simulation/Restraunt_Simulation/Restaurant.cpp
@@ -42,6 +42,14 @@ int Restaurant::GetCurrentTimestep() const
 {
 	return CurrTimeStep;
 }
+int Restaurant::getActionCounter() const
+{
+	return Action_Counter;
+}
+int Restaurant::getTotalActions() const
+{
+	return TotalActions;
+}
  void Restaurant::UpdateInterface()
 {
 	//Don't forget that you didn't add any variable for point 6 
@@ -93,6 +101,7 @@ Action* Restaurant::checkActions()
 		{
 			ActionList.dequeue(pAction);
 			pAction->Act();
+			Action_Counter++; 
 			return pAction;
 		}
 	} while (pAction->getTimeStep() == CurrTimeStep);
@@ -121,6 +130,7 @@ void Restaurant::FromPendingToCooking()
 	if (pOrder)
 	{
 		assignChefToOrderByType(pOrder);
+		pOrder->set_TF(pOrder->get_TR() + 1); // setting TF
 		return;
 	}
 
@@ -458,12 +468,12 @@ void Restaurant::FromReadyToInServ()
 
 
 	Ready_OT.peek(pOrder);
-	while(pOrder){
+	while(pOrder){ 
 		 
-		Ready_OT.dequeue(pOrder);
-		pOrder->set_TF(CurrTimeStep + 1);
-		Ready_OT.enqueue(pOrder);
-		Ready_OT.peek(pOrder);
+		Ready_OT.dequeue(pOrder); 
+		pOrder->set_TF(CurrTimeStep + 1); 
+		Ready_OT.enqueue(pOrder); 
+		Ready_OT.peek(pOrder); 
 	} 
 
 }
